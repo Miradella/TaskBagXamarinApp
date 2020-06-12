a49d23e299ff2ca3ad3f3ba956807ce6af092e7a
Nistratova <nistratova97@mail.ru>
2020-06-12 17:41:48 +0400

    Добавлены тесты

--- TestBag/Tests.cs
+++ TestBag/Tests.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using NUnit.Framework;
 using Xamarin.UITest;
 using Xamarin.UITest.Queries;
+using TaskBag;
 
 namespace TestBag
 {
+   
+
     [TestFixture(Platform.Android)]
-    [TestFixture(Platform.iOS)]
+    //[TestFixture(Platform.iOS)]
     public class Tests
     {
         IApp app;
@@ -26,12 +29,48 @@ namespace TestBag
         }
 
         [Test]
-        public void WelcomeTextIsDisplayed()
+        public void ViewRepl()
         {
-            AppResult[] results = app.WaitForElement(c => c.Marked("Welcome to Xamarin.Forms!"));
-            app.Screenshot("Welcome screen.");
+            app.Repl();
+        }
 
-            Assert.IsTrue(results.Any());
+        [Test]
+        public void InitializeNote_CheckFields()
+        {
+            string noteName = "Заголовок";
+            string noteText = "Описание";
+            Note note = new Note();
+            note.Note_Name = noteName;
+            note.Text = noteText;
+            StringAssert.Contains(note.Note_Name, noteName);
+            StringAssert.Contains(note.Text, noteText);
+        }
+
+
+        [Test]
+        public void AddNote_in_list()
+        {
+            app.Query(c => c.Id("toolbar"));
+            app.Tap(c => c.Marked("Add"));
+            app.EnterText(c => c.Marked("NoResourceEntry-14"), "Title1");
+            app.EnterText(c => c.Marked("NoResourceEntry-15"), "Description1");
+            app.DismissKeyboard();
+            app.Tap(c => c.Marked("Save"));
+        }
+
+        [Test]
+        public void Login()
+        {
+            app.Query(c => c.Id("toolbar"));
+            app.Tap(c => c.Marked("Ещё"));
+            app.Tap(c => c.Marked("title"));
+            app.EnterText(c => c.Marked("NoResourceEntry-12"), "email@new.com");
+            app.EnterText(c => c.Marked("NoResourceEntry-13"), "rrrrr5");
+            app.DismissKeyboard();
+            app.Tap(c => c.Marked("SingUp"));
+            app.Tap(c => c.Marked("Login"));
         }
     }
+
 }
+
